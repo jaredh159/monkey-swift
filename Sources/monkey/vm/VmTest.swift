219810d7030e56f2b6abfbd139c9395b3fa18894
Jared Henderson <jared@netrivet.com>
2021-05-03 09:17:48 -0400

    add test for recursive fibb

--- Sources/monkey/vm/VmTest.swift
+++ Sources/monkey/vm/VmTest.swift
@@ -527,6 +527,27 @@ func testVm() -> Bool {
     ])
   }
 
+  test("recursive fibonacci") {
+    runVmTests([
+      (
+        """
+        let fibonacci = fn(x) {
+          if (x == 0) {
+            return 0;
+          } else {
+            if (x == 1) {
+              return 1;
+            } else {
+              fibonacci(x - 1) + fibonacci(x - 2);
+            }
+          }
+        };
+        fibonacci(15);
+        """,
+        610
+      )
+    ])
+  }
   return Test.report()
 }
 
